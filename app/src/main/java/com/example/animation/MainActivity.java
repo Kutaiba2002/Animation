bd7196d9acfcffd33aed360bf01aaf8168e3d432
Kutaiba2002 <kingkutaiba36@gmail.com>
2023-06-07 03:01:38 +0300

    Animation

---
+++ app/src/main/java/com/example/animation/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.animation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+
+public class MainActivity extends AppCompatActivity {
+    private ImageView imageV;
+
+    private TextView text;
+    private Animation top,bottom;
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        imageV =  findViewById(R.id.imageV);
+        text = findViewById(R.id.txtBurger);
+
+        top = AnimationUtils.loadAnimation(this,R.anim.top_animation);
+        bottom = AnimationUtils.loadAnimation(this,R.anim.bottom_animation);
+
+        text.setAnimation(bottom);
+        imageV.setAnimation(top);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this,new_activity.class);
+                startActivity(intent);
+                finish();
+            }
+        },5000);
+
+    }
+}
